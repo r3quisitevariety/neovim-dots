56ebc05db76aea8d65e25a5a026cac11ddb49f7e
rqvrty <Onoruuu@gmail.com>
2026-05-11 00:22:48 -0400

    fix rose pine theming transparency issues

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -3,7 +3,8 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
-		require("rose-pine").setup({ variant = "moon" })
+		require("rose-pine").setup({ variant = "moon", extend_background_behind_borders = false })
 		vim.cmd("colorscheme rose-pine")
+		require("transparent").clear()
 	end
 }
