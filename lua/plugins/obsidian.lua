c82b514ed4685075c335a56cd23f9b6e0ee0c983
rqvrty <Onoruuu@gmail.com>
2026-05-13 15:47:38 -0400

    add date time timestamp to obsidian.nvim

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,30 +1,36 @@
 return {
-  "obsidian-nvim/obsidian.nvim",
-  version = "*",
-  keys = {
-    { "<Leader>od", "<Cmd>Obsidian dailies<CR>", desc = "obsidian daily notes" },
-    { "<C-S-d>", "<C-r>=strftime('%A, %B %d, %Y, %I:%M%p')<CR><CR>", desc = "obsidian time stamp" },
-  },
+	"obsidian-nvim/obsidian.nvim",
+	version = "*",
+	keys = {
+		{ "<Leader>od", "<Cmd>Obsidian dailies<CR>", desc = "obsidian daily notes" },
+		{
+			"<leader>dt",
+			function()
+				local date_str = os.date("%A, [[%B %-d, %Y]], %I:%M%p")
+				vim.api.nvim_put({ date_str }, "l", true, true)
+			end,
+			desc = "Insert date-time stamp",
+		},
+	},
 
-  opts = {
-    legacy_commands = false, -- this will be removed in 4.0.0
-    workspaces = {
-      {
-        name = "obsidian",
-        path = "~/Documents/obsidian",
-      },
-    },
+	opts = {
+		legacy_commands = false,
+		workspaces = {
+			{
+				name = "obsidian",
+				path = "~/Documents/obsidian",
+			},
+		},
 
-    ui = {
-      enable = false, -- Disables checkboxes, links, and other visual enhancements
-    },
+		ui = {
+			enable = false,
+		},
 
-    daily_notes = {
-      enabled = true,
-      folder = "zzz/dailynotes",
-      date_format = "%B %-d, %Y",
-      default_tags = "type/dailynotes",
-    },
-
-  },
+		daily_notes = {
+			enabled = true,
+			folder = "zzz/dailynotes",
+			date_format = "%B %-d, %Y",
+			default_tags = "type/dailynotes",
+		},
+	},
 }
