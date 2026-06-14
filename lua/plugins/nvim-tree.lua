8eb3086d49e0b18d8e64574736cb02bb5fde8967
rqvrty <Onoruuu@gmail.com>
2026-06-14 18:49:22 -0400

    make nvim tree auto poll file changes

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -9,6 +9,11 @@ return {
     { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle nvim-tree" },
   },
   config = function()
-    require("nvim-tree").setup {}
+    require("nvim-tree").setup {
+      update_focused_file = {
+        enable = true,
+        update_root = true,
+      },
+    }
   end,
 }
