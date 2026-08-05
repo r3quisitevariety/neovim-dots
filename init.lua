6575e80fe63b959fb288cde1e56fbbab2ffb629d
r3quisitevariety <x3roo@proton.me>
2026-08-05 16:54:09 -0400

    clean up documentation, add missing config to treesitter

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.g.mapleader = " "
 vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.tabstop = 2 -- Visual width of a tab character
 vim.opt.shiftwidth = 2 -- Width for auto-indentation (>>/<<)
-vim.opt.softtabstop = 2 -- Number of spaces a Tab key press counts forCopied!
+vim.opt.softtabstop = 2 -- Number of spaces a Tab key press counts for
 
 vim.opt.linebreak = true
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,17 @@ return {
 	build = ":TSUpdate",
 	event = { "BufReadPre", "BufNewFile" },
 	opts = {
-		ensure_installed = { "markdown", "markdown_inline", "nix", "rust", "javascript", "typescript", "go" },
+		ensure_installed = {
+			"lua",
+			"tsx",
+			"markdown",
+			"markdown_inline",
+			"nix",
+			"rust",
+			"javascript",
+			"typescript",
+			"go",
+		},
 		highlight = { enable = true },
 	},
 }
