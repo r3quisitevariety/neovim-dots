4db706a8cdfd5abf22880da3394c49732d78f81a
r3quisitevariety <x3roo@proton.me>
2026-08-09 14:40:20 -0400

    make neovim transparent

--- init.lua
+++ init.lua
@@ -29,3 +29,26 @@ vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "VimResume" }, {
 })
 require("oil").setup()
 require("ibl").setup() -- indent blanklines
+
+-- makes neovim transparent
+local function make_transparent()
+	local groups = {
+		"Normal",
+		"NormalNC",
+		"NormalFloat",
+		"SignColumn",
+		"StatusLine",
+		"StatusLineNC",
+		"EndOfBuffer",
+		"NeoTreeNormal",
+		"NeoTreeNormalNC",
+	}
+	for _, group in ipairs(groups) do
+		vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
+	end
+end
+make_transparent()
+-- Reapply on colorscheme change to maintain transparency
+vim.api.nvim_create_autocmd("ColorScheme", {
+	callback = make_transparent,
+})
