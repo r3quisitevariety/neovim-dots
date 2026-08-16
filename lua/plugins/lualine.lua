cd8415faaa5b09f9c8785fe8e7babff7352fba94
r3quisitevariety <x3roo@proton.me>
2026-08-16 00:05:15 -0400

    make lualine transparent, update config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,22 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	opts = {
-		options = {
-			theme = "auto",
+	opts = function(_, opts)
+		local theme = require("lualine.themes.auto")
+		local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+		local foreground = normal.fg and string.format("#%06x", normal.fg) or "NONE"
+
+		for _, mode in pairs(theme) do
+			for _, section in pairs(mode) do
+				section.bg = "NONE"
+				section.fg = foreground
+			end
+		end
+
+		opts.options = vim.tbl_deep_extend("force", opts.options or {}, {
+			theme = theme,
 			section_separators = "",
 			component_separators = "|",
-		},
-	},
+		})
+		return opts
+	end,
 }
