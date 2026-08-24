d6874900a84ca4487a6a0e898e6cc961fb0d5da1
r3quisitevariety <x3roo@proton.me>
2026-08-24 13:23:14 -0400

    replace telescope ff + fg with snacks instead

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,14 +1,28 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
-    keys = {
-        { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-        { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep (ripgrep)" },
-        { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-        { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
-    },
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        -- optional but recommended
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    }
+	"nvim-telescope/telescope.nvim",
+	version = "*",
+	keys = {
+		-- replaced telescopes implementation with snacks cuz its better
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find files",
+		},
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Live grep (ripgrep)",
+		},
+		{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+		{ "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- optional but recommended
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 }
