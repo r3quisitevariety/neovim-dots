490325fecc8597a068ac3c58724956e1dbb6bd3e
r3quisitevariety <x3roo@proton.me>
2026-08-24 03:50:51 -0400

    add git integration stuff

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,4 +1,31 @@
 return {
 	"lewis6991/gitsigns.nvim",
-	opts = {},
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		current_line_blame = true,
+		current_line_blame_opts = {
+			delay = 500,
+			virt_text_pos = "eol",
+		},
+		current_line_blame_formatter = "<author>, <author_time:%R> • <summary>",
+
+		on_attach = function(bufnr)
+			local gs = require("gitsigns")
+			local map = function(mode, lhs, rhs, desc)
+				vim.keymap.set(mode, lhs, rhs, {
+					buffer = bufnr,
+					desc = desc,
+				})
+			end
+
+			map("n", "]h", gs.next_hunk, "Next changed hunk")
+			map("n", "[h", gs.prev_hunk, "Previous changed hunk")
+
+			map("n", "<leader>hp", gs.preview_hunk, "Preview changed hunk")
+			map("n", "<leader>hb", gs.blame_line, "Blame current line")
+			map("n", "<leader>hd", gs.diffthis, "Diff current file")
+			map("n", "<leader>hs", gs.stage_hunk, "Stage hunk")
+			map("n", "<leader>hr", gs.reset_hunk, "Reset hunk")
+		end,
+	},
 }
