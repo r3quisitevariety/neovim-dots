4dab15b6b3a3f441cf621e7e143898a9bf6d66ce
r3quisitevariety <x3roo@proton.me>
2026-09-02 02:06:21 -0400

    make init.lua MORE prettier

--- init.lua
+++ init.lua
@@ -1,68 +1,72 @@
--- IMPORTS --
-require("config/lazy")
-require("config/lsp")
-require("oil").setup()
-require("ibl").setup() -- indent blanklines
-
--- OPTIONS --
-vim.opt.swapfile = false -- bye bye swapfiles
-vim.opt.undofile = true -- session-persistent undo history
-vim.opt.relativenumber = true
-vim.opt.number = true
 vim.g.mapleader = " "
+
+-- imports
+require("config.lazy")
+require("config.lsp")
+require("oil").setup()
+require("ibl").setup()
+
+-- options
+vim.opt.swapfile = false
+vim.opt.undofile = true
+vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
-vim.opt.expandtab = true -- use literal spaces instead of tabs
-vim.opt.tabstop = 2 -- visual width of a tab
-vim.opt.shiftwidth = 2 -- width for auto-indentation (>>/<<)
-vim.opt.softtabstop = 2 -- number of spaces a Tab key press counts for
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.opt.linebreak = true
 vim.opt.autoread = true
 vim.opt.termguicolors = true
 
--- SHORTCUTS/MISC --
-vim.keymap.set("n", "<leader>w", "<cmd>set wrap!<cr>", { desc = "Toggle word wrap" })
+-- keymaps
+vim.keymap.set("n", "<leader>w", "<cmd>set wrap!<cr>", {
+	desc = "Toggle word wrap",
+})
 
--- so i can do `1g` instead of `1gt`
+-- 1g instead of 1gt for tabs
 for tab = 1, 9 do
 	vim.keymap.set("n", tab .. "g", "<cmd>tabnext " .. tab .. "<cr>", {
 		desc = "Go to tab " .. tab,
 	})
 end
 
---autodetects changes in current file
+-- autodetects change in current file
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "VimResume" }, {
-	group = vim.api.nvim_create_augroup("auto-reload", { clear = true }),
+	group = vim.api.nvim_create_augroup("AutoReload", { clear = true }),
 	callback = function()
 		vim.cmd.checktime()
 	end,
 })
 
--- COSMETICS --
--- removes the ~ ~ ~ on the left side of neovim
+-- appearance
 vim.opt.fillchars:append({ eob = " " })
--- makes neovim transparent
-local function make_transparent()
-	local groups = {
-		"Normal",
-		"NormalNC",
-		"NormalFloat",
-		"SignColumn",
-		"StatusLine",
-		"StatusLineNC",
-		"EndOfBuffer",
-		"NeoTreeNormal",
-		"NeoTreeNormalNC",
-		"LineNr",
-		"CursorLineNr",
-		"LineNrAbove",
-		"LineNrBelow",
-	}
-	for _, group in ipairs(groups) do
+
+local transparent_groups = {
+	"Normal",
+	"NormalNC",
+	"NormalFloat",
+	"SignColumn",
+	"StatusLine",
+	"StatusLineNC",
+	"EndOfBuffer",
+	"NeoTreeNormal",
+	"NeoTreeNormalNC",
+	"LineNr",
+	"CursorLineNr",
+	"LineNrAbove",
+	"LineNrBelow",
+}
+
+local function set_transparent_background()
+	for _, group in ipairs(transparent_groups) do
 		vim.api.nvim_set_hl(0, group, { bg = "NONE", ctermbg = "NONE" })
 	end
 end
-make_transparent()
--- reapply on colorscheme change to maintain transparency
+
+set_transparent_background()
+
 vim.api.nvim_create_autocmd("ColorScheme", {
-	callback = make_transparent,
+	callback = set_transparent_background,
 })
